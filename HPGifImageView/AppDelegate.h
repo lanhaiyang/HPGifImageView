1022020834f956576ce42628ab895f193014d2de
lanhaiyang <hepeng@hepengdembp.lan>
2017-05-28 11:32:25 +0800

    Initial Commit

---
+++ HPGifImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HPGifImageView
+//
+//  Created by 何鹏 on 17/5/28.
+//  Copyright © 2017年 何鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
